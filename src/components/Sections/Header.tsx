3b6d9fd0835a5a11a09ffe99e241095482f4ad9f
FullStackDev <whitehorse9021@gmail.com>
2023-04-17 14:14:34 -0500

    Code cleaning

--- src/components/Sections/Header.tsx
+++ src/components/Sections/Header.tsx
@@ -70,7 +70,7 @@ const MobileNav: FC<{navSections: SectionId[]; currentSection: SectionId | null}
       <>
         <button
           aria-label="Menu Button"
-          className="fixed top-2 right-2 z-40 rounded-md bg-orange-500 p-2 ring-offset-gray-800/60 hover:bg-orange-400 focus:outline-none focus:ring-0 focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2 sm:hidden"
+          className="fixed right-2 top-2 z-40 rounded-md bg-orange-500 p-2 ring-offset-gray-800/60 hover:bg-orange-400 focus:outline-none focus:ring-0 focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2 sm:hidden"
           onClick={toggleOpen}>
           <MenuAlt3Icon className="h-8 w-8 text-white" />
           <span className="sr-only">Open sidebar</span>
@@ -134,4 +134,5 @@ const NavItem: FC<{
 });
 
 Header.displayName = 'Header';
+
 export default Header;
